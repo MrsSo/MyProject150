b8c3871c3bf948ee7eae52a6c1fae60c9e34cf90
MrsSo <ss1001291@otc.edu>
2025-01-30 18:20:52 -0600

    calcautor

--- Program.cs
+++ Program.cs
@@ -1,29 +1,54 @@
-namespace Player;
+﻿
+[TestClass]
+public class CalculatorTests
+{
+    private CalculatorTests? _calculator;
 
-    public class Program 
+    [TestInitialize]
+    public void Setup()
     {
-        public Vector3 Position { get; set; }
-        public float speed { get; set; }
-        public float JumpForce { get; set; }
-
-
-
-        public void Move(float horizontalInput) 
-        {
-            Position.x += horizontalInput * speed * Time.deltaTime;
-
-        }
-
-
-        public void Jump()
-        {
-            if (IsGrounded())
-            {
-                Position.y += JumpForce;
-
-            }
-        }
-
-
-    
+        // Arrange
+        _calculator = new Calculator();
     }
+
+    [TestMethod]
+    public void Add_ShouldReturnSum_WhenGivenTwoPositiveNumbers()
+    {
+        // Act
+        var result = _calculator.Add(5, 7);
+
+        // Assert
+        Assert.AreEqual(12, result);
+    }
+
+    private object Add(int v1, int v2)
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void Add_ShouldReturnSum_WhenGivenNegativeAndPositiveNumber()
+    {
+        // Act
+        var result = _calculator.Add(-3, 7);
+
+        // Assert
+        Assert.AreEqual(4, result);
+    }
+}
+
+internal class TestMethodAttribute : Attribute
+{
+}
+
+internal class Calculator : CalculatorTests
+{
+}
+
+internal class TestInitializeAttribute : Attribute
+{
+}
+
+internal class TestClassAttribute : Attribute
+{
+}
