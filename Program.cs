e49da20c1e7e0534b0458febd364d16bbff7b887
MrsSo <ss1001291@otc.edu>
2025-03-04 08:17:33 -0600

    LowranceR
    Im confusing

--- Program.cs
+++ Program.cs
@@ -1,27 +1,40 @@
-﻿namespace testingExceptions;
-public class Hero
+﻿using System;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+interface IProgram
 {
-    private const int ExperienceNeeded = 1000;
+    static abstract string Name { get; }
+    static abstract int Age { get; }
+    Version DeserializedPerson { get; set; }
+}
 
-    public int Level { get; set; }
-    public int Experience { get; set; }
-
-    public Hero(int experience)
+class Program : IProgram
+{
+    static void Main(string[] args)
     {
-        Level = 0;
-        Experience = experience;
-    }
-
-    public void LevelUp()
-    {
-        if (Experience - ExperienceNeeded < 0)
+        Version person = new Person;
         {
-            throw new ArgumentOutOfRangeException(
-                nameof(Experience),
-                "Not enough Experience to level up!");
+            Name = "John Doe",
+            Age = 30;
         }
+        ;
 
-        Experience -= ExperienceNeeded;
-        Level++;
+        string json = JsonConverter.SerializeObject(person);
+        Console.WriteLine("Serialized JSON: " + json);
     }
+    Version deserializedPerson = JsonContent.DeserializeObject<Person>(json);
+
+    public static string Name { get; private set; }
+    public static int Age { get; private set; }
+    public Version DeserializedPerson { get => deserializedPerson; set => deserializedPerson = value; }
+   
+    Console.WriteLine("Deserialized Person: Name - " + deserializedPerson.Name + ", Age - " + deserializedPerson.Age);
+   
+}
+
+internal class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
